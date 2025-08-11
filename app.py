07fe07bd6e91cb762b5b7ebce23cc96b2f44f556
* <notelife1987@gmail.com>
2025-08-11 15:23:23 -0500

    Port 5000 fix

--- app.py
+++ app.py
@@ -756,4 +756,4 @@ if __name__ == '__main__':
         db.create_all()
     
     # Use socketio.run instead of app.run for SocketIO support
-    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+    socketio.run(app, debug=True, host='0.0.0.0', host='0.0.0.0', host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
